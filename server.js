a26c264644d923faa067c9357cc55c1d937b93f3
camyacodes <camyacodes@gmail.com>
2022-02-20 21:55:22 -0500

    building database calls

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'C$ocks2016!',
+    database: 'employee_tracker'
+  },
+  console.log('Connected to the employee_tracker database.')
+);
+
+db.query(`SELECT * FROM departments`, (err, rows) => {
+  console.log(rows);
+});
+
 
   // Default response for any other request (Not Found)
 app.use((req, res) => {
