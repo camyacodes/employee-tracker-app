719932e28b5151748e384d4fe5141aa40df719ea
camyacodes <camyacodes@gmail.com>
2022-03-27 18:16:10 -0400

    employees schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
 
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -14,4 +16,16 @@ CREATE TABLE roles (
     CONSTRAINT fk_dept 
     FOREIGN KEY (department_id) 
     REFERENCES departments(id)
+    ON DELETE SET NULL
     );
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR (50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  CONSTRAINT fk_role 
+  FOREIGN KEY (role_id) 
+  REFERENCES roles(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,16 @@ VALUES
     ('Baker', 50000, 2),
     ('Pharmacist', 78000, 3),
     ('Customer Care Associate', 35000, 1);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Lewys', 'Everett', 1, 1),
+    ('Idrees', 'Harper', 2, 2),
+    ('Kieran', 'Timms', 1, 2),
+    ('Kayla', 'Baldwin', 3, 2),
+    ('Ammar', 'East', 3, 2),
+    ('Honor', 'Markham', 2, 2),
+    ('Samirah', 'Johnston', 1, 2),
+    ('Zayd', 'Everett', 3, 2);
+    
+
